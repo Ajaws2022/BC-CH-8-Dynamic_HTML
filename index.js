c78775ce63c207c77d890ac6ac82d9d1da975d44
Ajaws2022 <112523117+Ajaws2022@users.noreply.github.com>
2022-11-22 17:25:44 -0800

    added class modules for the employee types in the appropriate.js files and began adding questions for each type in index.js

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ const newTeam = [];
 // THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
 // WHEN I enter the team manager’s name, employee ID, email address, and office number
 // THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-const questions = async () => {
-    const answers = await inquirer
+const managerQuestions = async () => {
+    let answers = await inquirer
      .prompt([
         { 
             type: 'input',
@@ -34,5 +34,52 @@ const questions = async () => {
         }
      ])
         // add manager js here to create manager
+        const manager = new Manager(
+            answers.name,
+            answers.id,
+            answers.email,
+            answers.office
+        )
+        newTeam.push(manager)
 }
 
+const engQuestions = async () => {
+  let answers = await inquirer
+   .prompt([
+    { 
+        type: 'input',
+        message: "Please enter the engineer's name:",
+        name: 'name',
+    },
+    {
+        type: 'input',
+        message: "Please enter the engineer's employee ID:",
+        name: 'id',
+    },
+    {
+        type: 'input',
+        message: "Please enter the engineer's email address:",
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: "Please enter the engineer's GitHub username:",
+        name: 'github', 
+    }
+   ])
+
+   const engineer = new Engineer (
+    answers.name,
+    answers.id,
+    answers.email,
+    answers.github
+   )
+   newTeam.push(engineer)
+}
+
+
+const teamQuestions = async () => {
+    // await managerQuestions()
+
+    // add prompt to ask if adding engineer or
+}
